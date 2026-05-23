0173afb572aa2e188b97c96e4d9711a72d15b380
devs001 <devendersandhu001@gmail.com>
2026-05-23 15:17:06 +0530

    post cancel all order by id 4

--- demo_pro/myapp/views.py
+++ demo_pro/myapp/views.py
@@ -21,7 +21,7 @@ from .serializers import (
     InvoiceSerializer
 )
 from .tasks import create_invoice_for_subscription, verify_webhook, log, get_open_position, place_market_order, \
-    place_sl_with_retry, QUANTITY, cancel_all_orders
+    place_sl_with_retry, QUANTITY, cancel_all_orders, cancel_all_orders_order_id
 import stripe
 from django.conf import settings
 
@@ -165,7 +165,7 @@ class WebhookView(APIView):
 
                 close_side = "sell" if float(position["size"]) > 0 else "buy"
                 result     = place_market_order(close_side, QUANTITY)
-                can_res=cancel_all_orders()
+                can_res=cancel_all_orders_order_id()
                 log.info(f"Position closed: {result} cancel res {can_res}")
 
                 return Response({"status": "closed"})
